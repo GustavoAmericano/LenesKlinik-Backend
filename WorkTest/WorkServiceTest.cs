59007551bd8d0d3a55754a04a74a9354021c8faf
Kenneth P <quizandk@gmail.com>
2018-11-28 10:34:54 +0100

    Refactored WorkServiceTest
    Made a constructor to create the Mock, service and list of mockwork, as we now run with instance variables, rather than locals, for these.

--- WorkTest/WorkServiceTest.cs
+++ WorkTest/WorkServiceTest.cs
@@ -10,23 +10,22 @@ using Xunit;
 
 namespace WorkTest
 {
-#region CREATE
     public class WorkServiceTest
     {
-        [Fact]
-        public void CreateWorkSuccessTest()
+        private readonly List<Work> _work;
+        private readonly IWorkService _service;
+        private readonly Mock<IWorkRepository> mock;
+
+        // This constructor is called for each test, so that they may run simultaneously
+        public WorkServiceTest()
         {
+            _work = GetMockWork().ToList();
+            mock = new Mock<IWorkRepository>();
+            _service = new WorkService(mock.Object);
+            Work w = _work[0];
 
-            Work w = new Work
-            {
-                Title = "Massage",
-                Description = "A nice massage on location",
-                Duration = 30,
-                Price = 200
-            };
-
-            var mock = new Mock<IWorkRepository>();
-            IWorkService service = new WorkService(mock.Object);
+            mock.Setup(repo => repo.DeleteWork(It.IsAny<int>()));
+            mock.Setup(repo => repo.GetAllWork()).Returns(GetMockWork);
             mock.Setup(repo => repo.CreateWork(It.IsAny<Work>())).Returns(new Work
             {
                 Id = 1,
@@ -36,8 +35,14 @@ namespace WorkTest
                 Price = w.Price,
                 ImageUrl = "url.png"
             });
-            var returnWork = service.CreateWork(w);
 
+        }
+        #region CREATE
+        [Fact]
+        public void CreateWorkSuccessTest()
+        {
+            Work w = _work[0];
+            var returnWork = _service.CreateWork(w);
             mock.Verify(repo => repo.CreateWork(w), Times.Once);
             Assert.Equal(1, returnWork.Id);
         }
@@ -45,99 +50,64 @@ namespace WorkTest
         [Fact]
         public void CreateWorkNoTitleExpectArgumentExceptionTest()
         {
-            IWorkService service = GetWorkService();
-            Work w = new Work
-            {
-                Description = "A nice massage on location",
-                Duration = 30,
-                Price = 200,
-                ImageUrl = "url.png"
-            };
-            Exception e = Assert.Throws<ArgumentException>(() => service.CreateWork(w));
-
+            Work w = _work[0];
+            w.Title = null;
+            Exception e = Assert.Throws<ArgumentException>(() => _service.CreateWork(w));
             Assert.Equal("Title empty or null!", e.Message);
+            mock.Verify(repo => repo.CreateWork(w), Times.Never);
         }
 
         [Fact]
         public void CreateWorkNoDescriptionExpectArgumentExceptionTest()
         {
-            IWorkService service = GetWorkService();
-            Work w = new Work
-            {
-                Title = "Massage",
-                Duration = 30,
-                Price = 200,
-                ImageUrl = "url.png"
-            };
-
-            Exception e = Assert.Throws<ArgumentException>(() => service.CreateWork(w));
+            Work w = _work[0];
+            w.Description = null;
+            Exception e = Assert.Throws<ArgumentException>(() => _service.CreateWork(w));
             Assert.Equal("Description empty or null!", e.Message);
+            mock.Verify(repo => repo.CreateWork(w), Times.Never);
         }
 
         [Theory]
         [InlineData(-1)]
         [InlineData(0)]
-        public void CreateWorkInvalidDurationExpectArgumentOutOfRangeExceptionTest(int duration)
+        public void CreateWorkInvalidDurationExpectArgumentExceptionTest(int duration)
         {
-            IWorkService service = GetWorkService();
-            Work w = new Work
-            {
-                Title = "Massage",
-                Description = "A nice massage",
-                Duration = duration,
-                Price = 200.00,
-                ImageUrl = "url.png"
-            };
-            Exception e = Assert.Throws<ArgumentOutOfRangeException>(() => service.CreateWork(w));
-            Assert.Contains("Duration cannot be 0 or less!", e.Message);
+            Work w = _work[0];
+            w.Duration = duration;
+            Exception e = Assert.Throws<ArgumentException>(() => _service.CreateWork(w));
+            Assert.Equal("Duration cannot be 0 or less!", e.Message);
+            mock.Verify(repo => repo.CreateWork(w), Times.Never);
         }
 
         [Theory]
         [InlineData(-1.00)]
         [InlineData(0.00)]
-        public void CreateWorkInvalidPriceExpectArgumentOutOfRangeExceptionTest(double price)
+        public void CreateWorkInvalidPriceExpectArgumentExceptionTest(double price)
         {
-            IWorkService service = GetWorkService();
-            Work w = new Work
-            {
-                Title = "Massage",
-                Description = "A nice massage",
-                Duration = 30,
-                Price = price,
-                ImageUrl = "url.png"
-            };
-            Exception e = Assert.Throws<ArgumentOutOfRangeException>(() => service.CreateWork(w));
-            Assert.Contains("Price cannot be 0 or less!", e.Message);
+            Work w = _work[0];
+            w.Price = price;
+            Exception e = Assert.Throws<ArgumentException>(() => _service.CreateWork(w));
+            Assert.Equal("Price cannot be 0 or less!", e.Message);
+            mock.Verify(repo => repo.CreateWork(w), Times.Never);
         }
-
-        #endregion
-
-#region READ
-        [Fact]
-        public void GetAllWorkSuccessTest()
-        {
-            var mock = new Mock<IWorkRepository>();
-            IWorkService service = new WorkService(mock.Object);
-            mock.Setup(repo => repo.GetAllWork()).Returns(GetMockWork);
-            var returnWork = service.GetAllWork();
-
-            mock.Verify(repo => repo.GetAllWork(), Times.Once);
-            Assert.Equal(2,returnWork.Count());
-        }
-
-
 
         #endregion
 
         #region READ
         [Fact]
+        public void GetAllWorkSuccessTest()
+        {
+            var returnWork = _service.GetAllWork();
+            mock.Verify(repo => repo.GetAllWork(), Times.Once);
+            Assert.Equal(2, returnWork.Count());
+        }
+        #endregion
+
+        #region DELETE
+        [Fact]
         public void DeleteWorkSuccessTest()
         {
-            var mock = new Mock<IWorkRepository>();
-            IWorkService service = new WorkService(mock.Object);
-            mock.Setup(repo => repo.DeleteWork(It.IsAny<int>()));
-            service.DeleteWork(1);
-
+            _service.DeleteWork(1);
             mock.Verify(repo => repo.DeleteWork(1), Times.Once);
         }
 
@@ -145,9 +115,7 @@ namespace WorkTest
         #endregion
         private IWorkService GetWorkService()
         {
-            var mock = new Mock<IWorkRepository>();
-            IWorkService service = new WorkService(mock.Object);
-            return service;
+            return null;
         }
 
         private IEnumerable<Work> GetMockWork()
@@ -176,5 +144,5 @@ namespace WorkTest
         }
 
     }
-    
+
 }
