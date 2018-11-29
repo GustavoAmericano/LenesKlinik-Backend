13b093f27cb763ee48bcdc129dd74b3f0ea4c0a7
Kenneth P <quizandk@gmail.com>
2018-11-29 11:02:36 +0100

    Created user entity
    Entity containing login and personal information.

---
+++ LenesKlinik.Core.Entities/User.cs
@@ -0,0 +1,17 @@
+﻿namespace LenesKlinik.Core.Entities
+{
+    public class User
+    {
+        // Login information
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
+
+        //Personal information
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public int SecretNumber { get; set; }
+    }
+}
